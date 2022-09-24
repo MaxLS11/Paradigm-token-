4644fdbeb1351fb393198999947eb6a9a29ee10c
Maxim <95439259+MaxLS11@users.noreply.github.com>
2022-09-24 19:44:44 +0200

    Update Paradigm.sol

--- Paradigm.sol
+++ Paradigm.sol
@@ -1,1 +1,25 @@
+// SPDX-License-Identifier: ChaingeFinance
+pragma solidity ^0.8.12;
 
+import "https://github.com/FUSIONFoundation/FRC759/blob/main/FRC759.sol"
+
+
+Contract Paradigm is FRC759 {
+
+Constructor {
+    
+
+
+}
+
+
+
+    function name() public view virtual override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view virtual override returns (string memory) {
+        return _symbol;
+    }
+    
+   }
